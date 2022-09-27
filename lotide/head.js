61dc0c22933d2994e44d871c2d35c6a367fb26ba
Abdulahi hirsi <110142610+ahirsi1215@users.noreply.github.com>
2022-09-27 19:57:31 -0400

    Delete lotide directory

--- lotide/head.js
+++
@@ -1,7 +0,0 @@
-const assertEqual = require('./assertEqual');
-
-const head = function(array) {
-  return array[0];
-};
-
-module.exports = head;
--- lotide/tail.js
+++
@@ -1,7 +0,0 @@
-const assertEqual = require('./assertEqual');
-
-const tail = function(array) {
-  return array.slice(1);
-};
-
-module.exports = tail;
